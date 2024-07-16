88a7d36cc6d14a0c66d7c608a8f9ba1a0538f631
vedant069 <vedantnadhe069@gmail.com>
2024-07-17 02:51:22 +0530

    data trans done

--- src/exception.py
+++ src/exception.py
@@ -28,10 +28,3 @@ class CustomException(Exception):
     
 
 
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by 0")
-        raise CustomException(e,sys)
-
--- test.py
+++ test.py
@@ -1,1 +1,2 @@
-print("skjbvnsjkbndjv skb")
+from setuptools import setup,find_packages
+setup(name='ml_project', version='1.0',packages=find_packages())
